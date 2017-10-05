fa27692ad81115f01d2a96dc912a4ff805b8b145
Victor Li <victorli5674@yahoo.com>
2017-10-04 23:48:17 -0700

    trying out custom themes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#21412a'
+  },
+  appBar: {
+    height: 50
+  }
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <App />
   </MuiThemeProvider>,
   document.getElementById('root')
