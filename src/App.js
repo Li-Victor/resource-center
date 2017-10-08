e6d9140a4aa87d0bd5c3edefa5d13692762ea961
Victor Li <victorli5674@yahoo.com>
2017-10-07 22:19:15 -0700

    refactor for theme

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import fusTheme from './fusTheme';
 
 class App extends Component {
   constructor(props) {
@@ -12,21 +14,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
-        />
-        <Drawer
-          docked={false}
-          open={this.state.open}
-          onRequestChange={open => this.setState({ open })}
-        >
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
+          />
+          <Drawer
+            docked={false}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
